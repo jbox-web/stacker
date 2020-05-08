b8fcdea63bfc511996d0d3f17649a61166c84e2c
Nicolas Rodriguez <nicoladmin@free.fr>
2020-05-08 02:15:42 +0200

    Extract test data from code

--- spec/stacker/utils_spec.cr
+++ spec/stacker/utils_spec.cr
@@ -7,35 +7,12 @@ describe Stacker::Utils do
       Stacker::Pillar(@container={"foo" => Stacker::Pillar(@container={"bar" => [1, "127.0.0.1", true], "is_true" => true, "is_false" => false, "is_array" => [], "is_hash" => Stacker::Pillar(@container={}), "nested" => Stacker::Pillar(@container={"foo" => Stacker::Pillar(@container={"bar" => [1, "127.0.0.1", false], "is_true" => true, "is_false" => false, "is_array" => [], "is_hash" => Stacker::Pillar(@container={})})})})})
       TPL
 
-      dump = <<-'TPL'
-      ---
-      foo:
-        bar:
-        - 1
-        - 127.0.0.1
-        - true
-        is_true: true
-        is_false: false
-        is_array: []
-        is_hash: {}
-        nested:
-          foo:
-            bar:
-            - 1
-            - 127.0.0.1
-            - false
-            is_true: true
-            is_false: false
-            is_array: []
-            is_hash: {}
-
-      TPL
-
-      yaml = YAML.parse(File.read("spec/fixtures/test.yml"))
+      yaml = YAML.parse(File.read("spec/fixtures/input/test.yml"))
       hash = Stacker::Utils.convert_hash(yaml.as_h)
+      generated_yaml = File.read("spec/fixtures/output/test.yml")
 
       hash.inspect.should eq(inspect)
-      YAML.dump(hash).should eq(dump)
+      YAML.dump(hash).should eq(generated_yaml)
     end
   end
 
@@ -45,76 +22,24 @@ describe Stacker::Utils do
       Stacker::Pillar(@container={"foo" => Stacker::Pillar(@container={"bar" => [1, "127.0.0.1", true], "is_true" => true, "is_false" => false, "is_array" => [], "is_hash" => Stacker::Pillar(@container={}), "nested" => Stacker::Pillar(@container={"foo" => Stacker::Pillar(@container={"bar" => [1, "127.0.0.1", false], "is_true" => true, "is_false" => false, "is_array" => [], "is_hash" => Stacker::Pillar(@container={})})})})})
       TPL
 
-      dump = <<-'TPL'
-      ---
-      foo:
-        bar:
-        - 1
-        - 127.0.0.1
-        - true
-        is_true: true
-        is_false: false
-        is_array: []
-        is_hash: {}
-        nested:
-          foo:
-            bar:
-            - 1
-            - 127.0.0.1
-            - false
-            is_true: true
-            is_false: false
-            is_array: []
-            is_hash: {}
-
-      TPL
-
-      file = "spec/fixtures/test.yml"
+      file = "spec/fixtures/input/test.yml"
       yaml = File.read(file)
       hash = Stacker::Utils.yaml_to_hash(yaml, file)
+      generated_yaml = File.read("spec/fixtures/output/test.yml")
 
       hash.inspect.should eq(inspect)
-      YAML.dump(hash).should eq(dump)
+      YAML.dump(hash).should eq(generated_yaml)
     end
   end
 
   describe ".deep_merge" do
     it "deep merges internal structure " do
-      dump = <<-'TPL'
-      ---
-      string: b string
-      integer: 2
-      is_true: false
-      is_false: true
-      old_key: foo
-      hash:
-        foo: bar
-        nested:
-          hash: bar
-          hash1: child1
-          array:
-          - foo
-          - bar
-          hash2: child2
-        bar: baz
-      array:
-      - string1
-      - 11
-      - true
-      - false
-      - string2
-      - 12
-      - false
-      - true
-      new_key: foo
-
-      TPL
-
-      hash1 = load_yaml("spec/fixtures/deep_merge_dict1.yml")
-      hash2 = load_yaml("spec/fixtures/deep_merge_dict2.yml")
+      hash1 = load_yaml("spec/fixtures/input/deep_merge_dict1.yml")
+      hash2 = load_yaml("spec/fixtures/input/deep_merge_dict2.yml")
+      generated_yaml = File.read("spec/fixtures/output/deep_merge.yml")
 
       Stacker::Utils.deep_merge!(hash1, hash2)
-      YAML.dump(hash1).should eq(dump)
+      YAML.dump(hash1).should eq(generated_yaml)
     end
   end
 end
