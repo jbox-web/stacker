0591956b6baa2062b25627b964623a57e4db923c
Nicolas Rodriguez <nicoladmin@free.fr>
2020-10-03 03:10:04 +0200

    Add Capistrano config samples for deployment

---
+++ example/config/deploy.rb
@@ -0,0 +1,6 @@
+# config valid for current version and patch releases of Capistrano
+lock '~> 3.14.0'
+
+set :application, 'saltstack-config'
+set :repo_url,    'repo url'
+set :deploy_to,   '/data/stacker'
---
+++ example/config/deploy/production.rb
@@ -0,0 +1,1 @@
+server 'salt.example.net', user: 'stacker', roles: %w{app db web}
