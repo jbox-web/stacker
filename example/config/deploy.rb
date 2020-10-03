ee928e41a2d3de55f349ab5e813605592bd33de9
Nicolas Rodriguez <nicoladmin@free.fr>
2020-10-03 05:00:02 +0200

    Improve Capistrano config example

--- example/config/deploy.rb
+++ example/config/deploy.rb
@@ -4,3 +4,18 @@ lock '~> 3.14.0'
 set :application, 'saltstack-config'
 set :repo_url,    'repo url'
 set :deploy_to,   '/data/stacker'
+
+append :linked_dirs, 'log'
+
+namespace :stacker do
+  desc 'Restart Stacker'
+  task :restart do
+    on roles(:all) do |host|
+      execute :sudo, 'systemctl', 'restart', 'stacker.service'
+    end
+  end
+end
+
+namespace :deploy do
+  after 'deploy:finished', 'stacker:restart'
+end
