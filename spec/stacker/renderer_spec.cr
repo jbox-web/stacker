46128d18d096d51019c0ccba19da91470e10276d
Nicolas Rodriguez <nicoladmin@free.fr>
2020-10-03 02:46:41 +0200

    Rename variable in spec to be more consistent

--- spec/stacker/renderer_spec.cr
+++ spec/stacker/renderer_spec.cr
@@ -6,26 +6,26 @@ end
 
 describe Stacker::Renderer do
   it "support array_push function" do
-    env = create_renderer
-    env.compile("spec/fixtures/array_push.j2", Hash(String, String).new).should eq("['foo', 'bar']")
+    renderer = create_renderer
+    renderer.compile("spec/fixtures/array_push.j2", Hash(String, String).new).should eq("['foo', 'bar']")
   end
 
   it "support merge_dict function" do
-    env = create_renderer
-    env.compile("spec/fixtures/merge_dict.j2", Hash(String, String).new).should eq("{'foo' => {'enabled' => true}, 'bar' => {'enabled' => true}}")
+    renderer = create_renderer
+    renderer.compile("spec/fixtures/merge_dict.j2", Hash(String, String).new).should eq("{'foo' => {'enabled' => true}, 'bar' => {'enabled' => true}}")
   end
 
   it "support (deep) merge_dict function" do
-    env = create_renderer
-    env.compile("spec/fixtures/deep_merge_dict.j2", Hash(String, String).new).should eq("{'string' => 'b string', 'integer' => 2, 'is_true' => false, 'is_false' => true, 'old_key' => 'foo', 'hash' => {'foo' => 'bar', 'nested' => {'hash' => 'bar', 'hash1' => 'child1', 'array' => ['foo', 'bar'], 'hash2' => 'child2'}, 'bar' => 'baz'}, 'array' => ['string1', 11, true, false, 'string2', 12, false, true], 'new_key' => 'foo'}")
+    renderer = create_renderer
+    renderer.compile("spec/fixtures/deep_merge_dict.j2", Hash(String, String).new).should eq("{'string' => 'b string', 'integer' => 2, 'is_true' => false, 'is_false' => true, 'old_key' => 'foo', 'hash' => {'foo' => 'bar', 'nested' => {'hash' => 'bar', 'hash1' => 'child1', 'array' => ['foo', 'bar'], 'hash2' => 'child2'}, 'bar' => 'baz'}, 'array' => ['string1', 11, true, false, 'string2', 12, false, true], 'new_key' => 'foo'}")
   end
 
   # See: https://github.com/crystal-lang/crystal/blob/master/spec/std/log/log_spec.cr#L147
   it "support log function" do
     backend = Log::MemoryBackend.new
     Stacker::Renderer::Log.backend = backend
-    env = create_renderer
-    env.compile("spec/fixtures/log.j2", Hash(String, String).new).should eq("")
+    renderer = create_renderer
+    renderer.compile("spec/fixtures/log.j2", Hash(String, String).new).should eq("")
     entry = backend.entries.first
     entry.source.should eq("renderer")
     entry.severity.should eq(s(:info))
@@ -34,14 +34,14 @@ describe Stacker::Renderer do
 
   it "support traverse filter" do
     data = {"id" => "foo", "server" => {"roles" => ["php", "nginx"]}}
-    env = create_renderer
-    env.compile("spec/fixtures/traverse.j2", {"pillar" => data}).should eq("foo\n['php', 'nginx']")
+    renderer = create_renderer
+    renderer.compile("spec/fixtures/traverse.j2", {"pillar" => data}).should eq("foo\n['php', 'nginx']")
   end
 
   it "support traverse filter with default value" do
     data = {"id" => "foo", "server" => {"roles" => nil}}
-    env = create_renderer
-    env.compile("spec/fixtures/traverse.j2", {"pillar" => data}).should eq("foo\n[]")
+    renderer = create_renderer
+    renderer.compile("spec/fixtures/traverse.j2", {"pillar" => data}).should eq("foo\n[]")
   end
 
   it "support dictsort filter" do
@@ -49,8 +49,8 @@ describe Stacker::Renderer do
     generated_data = File.read("spec/fixtures/output/dictsort_generated_data.yml").chomp
     generated_yaml = File.read("spec/fixtures/output/dictsort_generated_yaml.yml")
 
-    env = create_renderer
-    output = env.compile("spec/fixtures/dictsort.j2", {"pillar" => data})
+    renderer = create_renderer
+    output = renderer.compile("spec/fixtures/dictsort.j2", {"pillar" => data})
     output.should eq(generated_data)
 
     yaml = YAML.parse(output)
