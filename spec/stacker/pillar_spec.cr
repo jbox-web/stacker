adef42f49e9591f4cd76a5e13055df412267f8ca
Nicolas Rodriguez <nicoladmin@free.fr>
2020-11-13 01:19:12 +0100

    Improve specs and fixtures organization

--- spec/stacker/pillar_spec.cr
+++ spec/stacker/pillar_spec.cr
@@ -7,10 +7,10 @@ describe Stacker::Pillar do
       Stacker::Pillar(@container={"foo" => Stacker::Pillar(@container={"bar" => [1, "127.0.0.1", true], "is_true" => true, "is_false" => false, "is_array" => [], "is_hash" => Stacker::Pillar(@container={}), "nested" => Stacker::Pillar(@container={"foo" => Stacker::Pillar(@container={"bar" => [1, "127.0.0.1", false], "is_true" => true, "is_false" => false, "is_array" => [], "is_hash" => Stacker::Pillar(@container={})})})})})
       TPL
 
-      file = "spec/fixtures/input/test.yml"
+      file = "spec/fixtures/merge_strategies/input/test.yml"
       yaml = File.read(file)
       hash = Stacker::Pillar.yaml_to_pillar(yaml)
-      generated_yaml = File.read("spec/fixtures/output/test.yml")
+      generated_yaml = File.read("spec/fixtures/merge_strategies/output/test.yml")
 
       hash.inspect.should eq(inspect)
       YAML.dump(hash).should eq(generated_yaml)
@@ -23,9 +23,9 @@ describe Stacker::Pillar do
       Stacker::Pillar(@container={"foo" => Stacker::Pillar(@container={"bar" => [1, "127.0.0.1", true], "is_true" => true, "is_false" => false, "is_array" => [], "is_hash" => Stacker::Pillar(@container={}), "nested" => Stacker::Pillar(@container={"foo" => Stacker::Pillar(@container={"bar" => [1, "127.0.0.1", false], "is_true" => true, "is_false" => false, "is_array" => [], "is_hash" => Stacker::Pillar(@container={})})})})})
       TPL
 
-      yaml = YAML.parse(File.read("spec/fixtures/input/test.yml"))
+      yaml = YAML.parse(File.read("spec/fixtures/merge_strategies/input/test.yml"))
       hash = Stacker::Pillar.convert_hash(yaml.as_h)
-      generated_yaml = File.read("spec/fixtures/output/test.yml")
+      generated_yaml = File.read("spec/fixtures/merge_strategies/output/test.yml")
 
       hash.inspect.should eq(inspect)
       YAML.dump(hash).should eq(generated_yaml)
@@ -34,9 +34,9 @@ describe Stacker::Pillar do
 
   describe ".deep_merge" do
     it "deep merges internal structure " do
-      hash1 = load_yaml("spec/fixtures/input/deep_merge_dict1.yml")
-      hash2 = load_yaml("spec/fixtures/input/deep_merge_dict2.yml")
-      generated_yaml = File.read("spec/fixtures/output/deep_merge.yml")
+      hash1 = load_yaml("spec/fixtures/merge_strategies/input/deep_merge_dict1.yml")
+      hash2 = load_yaml("spec/fixtures/merge_strategies/input/deep_merge_dict2.yml")
+      generated_yaml = File.read("spec/fixtures/merge_strategies/output/deep_merge.yml")
 
       Stacker::Pillar.deep_merge!(hash1, hash2)
       YAML.dump(hash1).should eq(generated_yaml)
@@ -45,9 +45,9 @@ describe Stacker::Pillar do
     describe "hash merging strategies" do
       context "when strategy is merge last" do
         it "should merge last hash" do
-          hash1 = load_yaml("spec/fixtures/input/merge_strategy_hash/merge_last_1.yml")
-          hash2 = load_yaml("spec/fixtures/input/merge_strategy_hash/merge_last_2.yml")
-          generated_yaml = File.read("spec/fixtures/output/merge_strategy_hash/merge_last.yml")
+          hash1 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_hash/merge_last_1.yml")
+          hash2 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_hash/merge_last_2.yml")
+          generated_yaml = File.read("spec/fixtures/merge_strategies/output/merge_strategy_hash/merge_last.yml")
 
           Stacker::Pillar.deep_merge!(hash1, hash2)
           YAML.dump(hash1).should eq(generated_yaml)
@@ -56,9 +56,9 @@ describe Stacker::Pillar do
 
       context "when strategy is merge first" do
         it "should merge first hash" do
-          hash1 = load_yaml("spec/fixtures/input/merge_strategy_hash/merge_first_1.yml")
-          hash2 = load_yaml("spec/fixtures/input/merge_strategy_hash/merge_first_2.yml")
-          generated_yaml = File.read("spec/fixtures/output/merge_strategy_hash/merge_first.yml")
+          hash1 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_hash/merge_first_1.yml")
+          hash2 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_hash/merge_first_2.yml")
+          generated_yaml = File.read("spec/fixtures/merge_strategies/output/merge_strategy_hash/merge_first.yml")
 
           Stacker::Pillar.deep_merge!(hash1, hash2)
           YAML.dump(hash1).should eq(generated_yaml)
@@ -67,9 +67,9 @@ describe Stacker::Pillar do
 
       context "when strategy is remove" do
         it "should remove hash" do
-          hash1 = load_yaml("spec/fixtures/input/merge_strategy_hash/remove_1.yml")
-          hash2 = load_yaml("spec/fixtures/input/merge_strategy_hash/remove_2.yml")
-          generated_yaml = File.read("spec/fixtures/output/merge_strategy_hash/remove.yml")
+          hash1 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_hash/remove_1.yml")
+          hash2 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_hash/remove_2.yml")
+          generated_yaml = File.read("spec/fixtures/merge_strategies/output/merge_strategy_hash/remove.yml")
 
           Stacker::Pillar.deep_merge!(hash1, hash2)
           YAML.dump(hash1).should eq(generated_yaml)
@@ -78,9 +78,9 @@ describe Stacker::Pillar do
 
       context "when strategy is overwrite" do
         it "should overwrite hash" do
-          hash1 = load_yaml("spec/fixtures/input/merge_strategy_hash/overwrite_1.yml")
-          hash2 = load_yaml("spec/fixtures/input/merge_strategy_hash/overwrite_2.yml")
-          generated_yaml = File.read("spec/fixtures/output/merge_strategy_hash/overwrite.yml")
+          hash1 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_hash/overwrite_1.yml")
+          hash2 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_hash/overwrite_2.yml")
+          generated_yaml = File.read("spec/fixtures/merge_strategies/output/merge_strategy_hash/overwrite.yml")
 
           Stacker::Pillar.deep_merge!(hash1, hash2)
           YAML.dump(hash1).should eq(generated_yaml)
@@ -91,9 +91,9 @@ describe Stacker::Pillar do
     describe "array merging strategies" do
       context "when strategy is merge last" do
         it "should merge last array" do
-          hash1 = load_yaml("spec/fixtures/input/merge_strategy_array/merge_last_1.yml")
-          hash2 = load_yaml("spec/fixtures/input/merge_strategy_array/merge_last_2.yml")
-          generated_yaml = File.read("spec/fixtures/output/merge_strategy_array/merge_last.yml")
+          hash1 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_array/merge_last_1.yml")
+          hash2 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_array/merge_last_2.yml")
+          generated_yaml = File.read("spec/fixtures/merge_strategies/output/merge_strategy_array/merge_last.yml")
 
           Stacker::Pillar.deep_merge!(hash1, hash2)
           YAML.dump(hash1).should eq(generated_yaml)
@@ -102,9 +102,9 @@ describe Stacker::Pillar do
 
       context "when strategy is merge first" do
         it "should merge first array" do
-          hash1 = load_yaml("spec/fixtures/input/merge_strategy_array/merge_first_1.yml")
-          hash2 = load_yaml("spec/fixtures/input/merge_strategy_array/merge_first_2.yml")
-          generated_yaml = File.read("spec/fixtures/output/merge_strategy_array/merge_first.yml")
+          hash1 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_array/merge_first_1.yml")
+          hash2 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_array/merge_first_2.yml")
+          generated_yaml = File.read("spec/fixtures/merge_strategies/output/merge_strategy_array/merge_first.yml")
 
           Stacker::Pillar.deep_merge!(hash1, hash2)
           YAML.dump(hash1).should eq(generated_yaml)
@@ -113,9 +113,9 @@ describe Stacker::Pillar do
 
       context "when strategy is remove" do
         it "should remove array" do
-          hash1 = load_yaml("spec/fixtures/input/merge_strategy_array/remove_1.yml")
-          hash2 = load_yaml("spec/fixtures/input/merge_strategy_array/remove_2.yml")
-          generated_yaml = File.read("spec/fixtures/output/merge_strategy_array/remove.yml")
+          hash1 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_array/remove_1.yml")
+          hash2 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_array/remove_2.yml")
+          generated_yaml = File.read("spec/fixtures/merge_strategies/output/merge_strategy_array/remove.yml")
 
           Stacker::Pillar.deep_merge!(hash1, hash2)
           YAML.dump(hash1).should eq(generated_yaml)
@@ -124,9 +124,9 @@ describe Stacker::Pillar do
 
       context "when strategy is overwrite" do
         it "should overwrite array" do
-          hash1 = load_yaml("spec/fixtures/input/merge_strategy_array/overwrite_1.yml")
-          hash2 = load_yaml("spec/fixtures/input/merge_strategy_array/overwrite_2.yml")
-          generated_yaml = File.read("spec/fixtures/output/merge_strategy_array/overwrite.yml")
+          hash1 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_array/overwrite_1.yml")
+          hash2 = load_yaml("spec/fixtures/merge_strategies/input/merge_strategy_array/overwrite_2.yml")
+          generated_yaml = File.read("spec/fixtures/merge_strategies/output/merge_strategy_array/overwrite.yml")
 
           Stacker::Pillar.deep_merge!(hash1, hash2)
           YAML.dump(hash1).should eq(generated_yaml)
--- spec/stacker/renderer_spec.cr
+++ spec/stacker/renderer_spec.cr
@@ -5,122 +5,136 @@ private def s(value : Log::Severity)
 end
 
 describe Stacker::Renderer do
-  it "support array_push function" do
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/array_push.j2", Hash(String, String).new).should eq("['foo', 'bar']")
+  describe "functions" do
+    describe "array_push" do
+      it "support array_push function" do
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/functions/array_push.j2", Hash(String, String).new).should eq("['foo', 'bar']")
+      end
+    end
+
+    describe "log" do
+      # See: https://github.com/crystal-lang/crystal/blob/master/spec/std/log/log_spec.cr#L147
+      it "support log function" do
+        backend = Log::MemoryBackend.new
+        Stacker::Renderer::Log.backend = backend
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/functions/log.j2", Hash(String, String).new).should eq("")
+        entry = backend.entries.first
+        entry.source.should eq("renderer")
+        entry.severity.should eq(s(:info))
+        entry.message.should eq("foo")
+      end
+    end
+
+    describe "merge_dict" do
+      it "support merge_dict function" do
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/functions/merge_dict.j2", Hash(String, String).new).should eq("{'foo' => {'enabled' => true}, 'bar' => {'enabled' => true}}")
+      end
+
+      it "support (deep) merge_dict function" do
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/functions/deep_merge_dict.j2", Hash(String, String).new).should eq("{'string' => 'b string', 'integer' => 2, 'is_true' => false, 'is_false' => true, 'old_key' => 'foo', 'hash' => {'foo' => 'bar', 'nested' => {'hash' => 'bar', 'hash1' => 'child1', 'array' => ['foo', 'bar'], 'hash2' => 'child2'}, 'bar' => 'baz'}, 'array' => ['string1', 11, true, false, 'string2', 12, false, true], 'new_key' => 'foo'}")
+      end
+    end
   end
 
-  it "support merge_dict function" do
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/merge_dict.j2", Hash(String, String).new).should eq("{'foo' => {'enabled' => true}, 'bar' => {'enabled' => true}}")
-  end
+  describe "filters" do
+    describe "traverse" do
+      it "support traverse filter" do
+        data = {"id" => "foo", "server" => {"roles" => ["php", "nginx"]}}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse.j2", {"pillar" => data}).should eq("foo\n['php', 'nginx']")
+      end
 
-  it "support (deep) merge_dict function" do
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/deep_merge_dict.j2", Hash(String, String).new).should eq("{'string' => 'b string', 'integer' => 2, 'is_true' => false, 'is_false' => true, 'old_key' => 'foo', 'hash' => {'foo' => 'bar', 'nested' => {'hash' => 'bar', 'hash1' => 'child1', 'array' => ['foo', 'bar'], 'hash2' => 'child2'}, 'bar' => 'baz'}, 'array' => ['string1', 11, true, false, 'string2', 12, false, true], 'new_key' => 'foo'}")
-  end
+      it "support traverse filter with default value: nil" do
+        data = {"id" => "foo", "server" => {"foo" => "bar"}}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse/default_nil_1.j2", {"pillar" => data}).should eq("null")
+      end
 
-  # See: https://github.com/crystal-lang/crystal/blob/master/spec/std/log/log_spec.cr#L147
-  it "support log function" do
-    backend = Log::MemoryBackend.new
-    Stacker::Renderer::Log.backend = backend
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/log.j2", Hash(String, String).new).should eq("")
-    entry = backend.entries.first
-    entry.source.should eq("renderer")
-    entry.severity.should eq(s(:info))
-    entry.message.should eq("foo")
-  end
+      it "support traverse filter with default value: nil" do
+        data = {"id" => "foo", "server" => {"foo" => "bar"}}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse/default_nil_2.j2", {"pillar" => data}).should eq("null")
+      end
 
-  it "support traverse filter" do
-    data = {"id" => "foo", "server" => {"roles" => ["php", "nginx"]}}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse.j2", {"pillar" => data}).should eq("foo\n['php', 'nginx']")
-  end
+      it "support traverse filter with default value: true" do
+        data = {"id" => "foo", "server" => {"roles" => nil}}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse/default_true.j2", {"pillar" => data}).should eq("true")
+      end
 
-  it "support traverse filter with default value: nil" do
-    data = {"id" => "foo", "server" => {"foo" => "bar"}}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse/default_nil_1.j2", {"pillar" => data}).should eq("null")
-  end
+      it "support traverse filter with default value: false" do
+        data = {"id" => "foo", "server" => {"roles" => nil}}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse/default_false.j2", {"pillar" => data}).should eq("false")
+      end
 
-  it "support traverse filter with default value: nil" do
-    data = {"id" => "foo", "server" => {"foo" => "bar"}}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse/default_nil_2.j2", {"pillar" => data}).should eq("null")
-  end
+      it "support traverse filter with default value: []" do
+        data = {"id" => "foo", "server" => {"roles" => nil}}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse/default_array_empty.j2", {"pillar" => data}).should eq("[]")
+      end
 
-  it "support traverse filter with default value: true" do
-    data = {"id" => "foo", "server" => {"roles" => nil}}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse/default_true.j2", {"pillar" => data}).should eq("true")
-  end
+      it "support traverse filter with default value: {}" do
+        data = {"id" => "foo", "server" => {"roles" => nil}}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse/default_hash_empty.j2", {"pillar" => data}).should eq("{}")
+      end
 
-  it "support traverse filter with default value: false" do
-    data = {"id" => "foo", "server" => {"roles" => nil}}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse/default_false.j2", {"pillar" => data}).should eq("false")
-  end
+      it "support traverse filter with default value: ''" do
+        data = {"id" => "foo", "server" => {"roles" => nil}}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse/default_string_empty.j2", {"pillar" => data}).should eq("\"\"")
+      end
 
-  it "support traverse filter with default value: []" do
-    data = {"id" => "foo", "server" => {"roles" => nil}}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse/default_array_empty.j2", {"pillar" => data}).should eq("[]")
-  end
+      it "support traverse filter with default value: 'true'" do
+        data = {"id" => "foo", "server" => {"roles" => nil}}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse/default_string_true.j2", {"pillar" => data}).should eq("\"true\"")
+      end
 
-  it "support traverse filter with default value: {}" do
-    data = {"id" => "foo", "server" => {"roles" => nil}}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse/default_hash_empty.j2", {"pillar" => data}).should eq("{}")
-  end
+      it "support traverse filter with default value: 'false'" do
+        data = {"id" => "foo", "server" => {"roles" => nil}}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse/default_string_false.j2", {"pillar" => data}).should eq("\"false\"")
+      end
 
-  it "support traverse filter with default value: ''" do
-    data = {"id" => "foo", "server" => {"roles" => nil}}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse/default_string_empty.j2", {"pillar" => data}).should eq("\"\"")
-  end
+      it "support traverse filter with default value when traversal path doesn't exist (1)" do
+        backend = Log::MemoryBackend.new
+        Stacker::Renderer::Log.backend = backend
+        Stacker::Renderer::Log.level = ::Log::Severity::Debug
+        data = {"id" => "foo"}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse/traversal_path_inexistent_1.j2", {"pillar" => data}).should eq("\"\"")
+        entry = backend.entries.first
+        entry.source.should eq("renderer")
+        entry.severity.should eq(s(:debug))
+        entry.message.should eq("Attribute 'server:roles' not found in traversal path")
+      end
 
-  it "support traverse filter with default value: 'true'" do
-    data = {"id" => "foo", "server" => {"roles" => nil}}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse/default_string_true.j2", {"pillar" => data}).should eq("\"true\"")
-  end
+      it "support traverse filter with default value when traversal path doesn't exist (2)" do
+        data = {"id" => "foo", "server" => {"foo" => "bar"}}
+        renderer = create_renderer
+        renderer.compile("spec/fixtures/filters/traverse/traversal_path_inexistent_2.j2", {"pillar" => data}).should eq("\"\"")
+      end
+    end
 
-  it "support traverse filter with default value: 'false'" do
-    data = {"id" => "foo", "server" => {"roles" => nil}}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse/default_string_false.j2", {"pillar" => data}).should eq("\"false\"")
-  end
+    describe "dictsort" do
+      it "support dictsort filter" do
+        data = load_yaml("spec/fixtures/filters/dictsort/base.yml")
+        generated_data = File.read("spec/fixtures/filters/dictsort/dictsort_generated_data.yml").chomp
+        generated_yaml = File.read("spec/fixtures/filters/dictsort/dictsort_generated_yaml.yml")
 
-  it "support traverse filter with default value when traversal path doesn't exist (1)" do
-    backend = Log::MemoryBackend.new
-    Stacker::Renderer::Log.backend = backend
-    Stacker::Renderer::Log.level = ::Log::Severity::Debug
-    data = {"id" => "foo"}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse/traversal_path_inexistent_1.j2", {"pillar" => data}).should eq("\"\"")
-    entry = backend.entries.first
-    entry.source.should eq("renderer")
-    entry.severity.should eq(s(:debug))
-    entry.message.should eq("Attribute 'server:roles' not found in traversal path")
-  end
+        renderer = create_renderer
+        output = renderer.compile("spec/fixtures/filters/dictsort.j2", {"pillar" => data})
+        output.should eq(generated_data)
 
-  it "support traverse filter with default value when traversal path doesn't exist (2)" do
-    data = {"id" => "foo", "server" => {"foo" => "bar"}}
-    renderer = create_renderer
-    renderer.compile("spec/fixtures/traverse/traversal_path_inexistent_2.j2", {"pillar" => data}).should eq("\"\"")
-  end
-
-  it "support dictsort filter" do
-    data = load_yaml("spec/fixtures/input/base.yml")
-    generated_data = File.read("spec/fixtures/output/dictsort_generated_data.yml").chomp
-    generated_yaml = File.read("spec/fixtures/output/dictsort_generated_yaml.yml")
-
-    renderer = create_renderer
-    output = renderer.compile("spec/fixtures/dictsort.j2", {"pillar" => data})
-    output.should eq(generated_data)
-
-    yaml = YAML.parse(output)
-    YAML.dump(yaml).should eq(generated_yaml)
+        yaml = YAML.parse(output)
+        YAML.dump(yaml).should eq(generated_yaml)
+      end
+    end
   end
 end
