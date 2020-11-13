d221eab8f5f908f5f542d3fc74729dde2be9a2e5
Nicolas Rodriguez <nicoladmin@free.fr>
2020-11-13 02:39:59 +0100

    Move Crinja specific methods to merge_dict filter

--- src/runtime/function/merge_dict.cr
+++ src/runtime/function/merge_dict.cr
@@ -2,7 +2,37 @@ module Stacker::Function
   Crinja.function({hash: nil, other: nil}, :merge_dict) do
     hash = arguments["hash"]
     other = arguments["other"]
-    Stacker::Utils.deep_merge_crinja!(hash, other)
+    Stacker::Function.deep_merge_crinja!(hash, other)
     hash
+  end
+
+  def self.deep_merge_crinja!(hash, other_hash)
+    hash = hash.raw
+    other_hash = other_hash.raw
+
+    return unless hash.is_a?(Hash) && other_hash.is_a?(Hash)
+
+    other_hash.each do |current_key, other_value|
+      this_value = hash[current_key.to_s]?
+
+      hash[Crinja::Value.new(current_key.to_s)] =
+        if this_value.nil?
+          to_crinja_value(other_value)
+        elsif this_value.raw.is_a?(Hash) && other_value.raw.is_a?(Hash)
+          to_crinja_value deep_merge_crinja!(this_value, other_value)
+        elsif this_value.raw.is_a?(Array) && other_value.raw.is_a?(Array)
+          to_crinja_value(this_value.concat(other_value))
+        else
+          to_crinja_value(other_value)
+        end
+    end
+
+    hash
+  end
+
+  def self.to_crinja_value(value)
+    return value if value.is_a?(Crinja::Value)
+
+    Crinja::Value.new(value)
   end
 end
--- src/stacker/utils.cr
+++ src/stacker/utils.cr
@@ -12,36 +12,6 @@ module Stacker
       string.split("\n").reject(&.empty?)
     end
 
-    def self.deep_merge_crinja!(hash, other_hash)
-      hash = hash.raw
-      other_hash = other_hash.raw
-
-      return unless hash.is_a?(Hash) && other_hash.is_a?(Hash)
-
-      other_hash.each do |current_key, other_value|
-        this_value = hash[current_key.to_s]?
-
-        hash[Crinja::Value.new(current_key.to_s)] =
-          if this_value.nil?
-            to_crinja_value(other_value)
-          elsif this_value.raw.is_a?(Hash) && other_value.raw.is_a?(Hash)
-            to_crinja_value deep_merge_crinja!(this_value, other_value)
-          elsif this_value.raw.is_a?(Array) && other_value.raw.is_a?(Array)
-            to_crinja_value(this_value.concat(other_value))
-          else
-            to_crinja_value(other_value)
-          end
-      end
-
-      hash
-    end
-
-    def self.to_crinja_value(value)
-      return value if value.is_a?(Crinja::Value)
-
-      Crinja::Value.new(value)
-    end
-
     def self.crinja_info(env)
       [env.filters, env.tests, env.functions, env.tags, env.operators].each do |library|
         puts "#{library.name}s:"
