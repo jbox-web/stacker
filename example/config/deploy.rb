53b0d4ee5b9e6426fcf5915521eb208a88630676
Nicolas Rodriguez <nicoladmin@free.fr>
2021-01-27 02:49:28 +0100

    Improve example

--- example/config/deploy.rb
+++ example/config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.14.0'
+lock '~> 3.15.0'
 
 set :application, 'saltstack-config'
 set :repo_url,    'repo url'
@@ -10,12 +10,30 @@ append :linked_dirs, 'log'
 namespace :stacker do
   desc 'Restart Stacker'
   task :restart do
-    on roles(:all) do |host|
+    on roles(:stacker) do
       execute :sudo, 'systemctl', 'restart', 'stacker.service'
+    end
+  end
+end
+
+namespace :salt do
+  desc 'Refresh Salt Pillar'
+  task :refresh_pillar do
+    on roles(:salt) do
+      execute :salt, '"*"', '--timeout', '5', '--log-level', 'quiet', 'saltutil.refresh_pillar'
+    end
+  end
+
+  desc 'Refresh Salt Mine'
+  task :mine_update do
+    on roles(:salt) do
+      execute :salt, '"*"', '--timeout', '5', '--log-level', 'quiet', 'mine.update'
     end
   end
 end
 
 namespace :deploy do
   after 'deploy:finished', 'stacker:restart'
+  after 'deploy:finished', 'salt:refresh_pillar'
+  after 'deploy:finished', 'salt:mine_update'
 end
--- example/config/deploy/production.rb
+++ example/config/deploy/production.rb
@@ -1,1 +1,1 @@
-server 'salt.example.net', user: 'stacker', roles: %w{app db web}
+server 'salt.example.net', user: 'stacker', roles: %w{stacker salt}
