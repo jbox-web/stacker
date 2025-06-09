5d91ebb1a8a7cb56911f3c2f14d97a26d6db2bf9
Nicolas Rodriguez <nico@nicoladmin.fr>
2025-06-09 02:58:45 +0200

    Fix Kemal warning

--- src/kemal_patch.cr
+++ src/kemal_patch.cr
@@ -3,7 +3,7 @@ module Kemal
   # :nodoc:
   def self.display_startup_message(config, server)
     addresses = server.addresses.map { |address| "#{config.scheme}://#{address}" }.join ", "
-    log "[#{config.env}] Kemal is ready to lead at #{addresses} (PID: #{Process.pid})"
+    Log.info { "[#{config.env}] Kemal is ready to lead at #{addresses} (PID: #{Process.pid})" }
     SystemdNotify.new.ready
   end
 end
